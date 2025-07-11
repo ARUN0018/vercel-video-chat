22cdc9d6662b23f47840645fe7076e7fc06fa6ac
Arun Kumar <arungdr0018@gmail.com>
2025-07-11 22:03:07 +0530

    action buttons are added

---
+++ src/controls/buttons.tsx
@@ -0,0 +1,87 @@
+import type { VideoClient } from "@zoom/videosdk";
+import Image from "next/image";
+
+export const VideoButton = (props: {
+  client: React.RefObject<typeof VideoClient>;
+  isVideoMuted: boolean;
+  setIsVideoMuted: React.Dispatch<React.SetStateAction<boolean>>;
+  renderVideo: (event: {
+    action: "Start" | "Stop";
+    userId: number;
+  }) => Promise<void>;
+}) => {
+  const { client, isVideoMuted, setIsVideoMuted, renderVideo } = props;
+  const onCameraClick = async () => {
+    if (isVideoMuted) {
+      const mediaStream = client.current.getMediaStream();
+      setIsVideoMuted(false);
+      await mediaStream.startVideo();
+      await renderVideo({
+        action: "Start",
+        userId: client.current.getCurrentUserInfo().userId,
+      });
+    } else {
+      const mediaStream = client.current.getMediaStream();
+      await mediaStream.stopVideo();
+      setIsVideoMuted(true);
+      await renderVideo({
+        action: "Stop",
+        userId: client.current.getCurrentUserInfo().userId,
+      });
+    }
+  };
+  return (
+    <button className="video-button" onClick={onCameraClick}>
+      {isVideoMuted ? (
+        <Image src="/video-off.svg" alt={""} width={30} height={30} />
+      ) : (
+        <Image src="/video-on.svg" alt={""} width={30} height={30} />
+      )}
+    </button>
+  );
+};
+
+export const AudioButton = (props: {
+  client: React.RefObject<typeof VideoClient>;
+  isAudioMuted: boolean;
+  setIsAudioMuted: React.Dispatch<React.SetStateAction<boolean>>;
+}) => {
+  const { client, isAudioMuted, setIsAudioMuted } = props;
+
+  const onAudioClick = async () => {
+    const mediaStream = client.current.getMediaStream();
+    isAudioMuted
+      ? await mediaStream.unmuteAudio()
+      : await mediaStream.muteAudio();
+    setIsAudioMuted(client.current.getCurrentUserInfo().muted ?? true);
+  };
+  return (
+    <button className="audio-button" onClick={onAudioClick}>
+      {isAudioMuted ? (
+        <Image src="/mic-off.svg" alt={""} width={30} height={30} style={{}} />
+      ) : (
+        <Image
+          src="/mic-on.svg"
+          alt={""}
+          width={30}
+          height={30}
+          style={{ color: "red" }}
+        />
+      )}
+    </button>
+  );
+};
+
+export const CallButton = () => {
+  return (
+    <button className="audio-button" style={{ backgroundColor: "red" }}>
+      <Image
+        src="/call.svg"
+        alt={""}
+        width={30}
+        height={30}
+        style={{ fill: "white" }}
+      />
+    </button>
+  );
+};
