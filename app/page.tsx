ea4e182713c9319bf4a4acc72dd15212de214a9a
Arun Kumar <arungdr0018@gmail.com>
2025-08-06 13:21:29 +0530

    @ reference change to ../

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { CanvasWrapper } from "@/src/call/CanvasWrapper";
+import { CanvasWrapper } from "../src/call/CanvasWrapper";
 import Script from "next/script";
 
 export default function Page(props: { params: Promise<{ slug: string }> }) {
